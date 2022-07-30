ed4f9ef970e189ffd6dbda1c2bed873b1539ddd9
grtcha <vladap1336@gmail.com>
2022-07-30 20:27:48 +0300

    solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  let nStr = Math.abs(n).toString();
+  for (let i = nStr.length - 1; i >= 0; i -= 1) {
+    result += nStr[i];
+  }
+  return result;
 }
